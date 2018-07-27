94470dc04af2e8336649e70e020952fe703a4b78
Ayushaj96 <ayushaj007@gmail.com>
2018-07-27 23:21:36 +0530

    more codes

--- BasicMathsProblem/GCD&LCM.cpp
+++ BasicMathsProblem/GCD&LCM.cpp
@@ -10,7 +10,18 @@ int gcd(int a,int b)
 	else
 	gcd(b,a%b);	
 }
+int gcd1(int a, int b)
+{
 
+	while (b) {
+		int t = b;
+
+		b = a % b;
+		a = t;
+		cout<<a<<" ";
+	}
+  	return a;
+}
 
 int lcm(int a,int b)
 {
@@ -23,7 +34,7 @@ int main()
 	int a,b;
 	cin>>a>>b;
 	
-	cout<<gcd(a,b)<<endl;
-	cout<<lcm(a,b);
+	cout<<gcd1(a,b)<<endl;
+	//cout<<lcm(a,b);
 	return 0;
 }
---
+++ Heap/min&max Heap(comparator,function).cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<queue>
+#include<functional>
+#include<vector>
+using namespace std;
+
+
+class Fruit
+{
+	public:
+		int price;
+		string name;
+		
+		Fruit(int a,string b)
+		{
+			price=a;
+			name=b;
+		}
+};
+
+class myCompare{
+    public :
+     bool operator ()(int a,int b){
+         //cout<<"\nThe number is "<<a<<","<<b;
+         return a<b; //for maxheap
+         //a>b for minheap
+     }
+};
+
+class mycompare{
+	
+	public:
+		
+		bool operator()(Fruit a,Fruit b)
+		{
+			//if price is equal then sort lexiographically
+         //minheap based upon price and then name
+         if(a.price==b.price){
+             return a.name>b.name;
+         }
+         return a.price>b.price;
+		}
+	
+};
+
+int main()
+{
+	
+	 Fruit f1(180,"Mango");
+    Fruit f2(180,"Apple");
+    Fruit f3(150,"Guave");
+    
+    priority_queue<Fruit,vector<Fruit>,mycompare> pq;
+    pq.push(f1);
+    pq.push(f2);
+    pq.push(f3);
+
+    while(!pq.empty()){
+        Fruit f=pq.top();
+        pq.pop();
+        cout<<f.name<<endl;
+        cout<<f.price<<endl<<endl;
+    
+}
+
+
+int a[]={1,4,3,2,6,0,10,12};
+    int n=sizeof(a)/sizeof(a[0]);
+ 
+ priority_queue<int> pqmax;
+ 
+    for(int i=0;i<n;i++){
+        pqmax.push(a[i]);
+    }
+    cout<<"Maxheap \n";
+    while(!pqmax.empty()){
+        cout<<pqmax.top()<<" ";
+        pqmax.pop();
+    }
+    
+  priority_queue<int,vector<int>,greater<int> >  pqmax2; 
+  
+  for(int i=0;i<n;i++){
+        pqmax2.push(a[i]);
+    }
+    
+    cout<<"\nMinheap \n";
+    while(!pqmax2.empty()){
+        cout<<pqmax2.top()<<" ";
+        pqmax2.pop();
+    }
+  
+  priority_queue<int,vector<int>,myCompare >  pqmax3; 
+  
+  for(int i=0;i<n;i++){
+        pqmax3.push(a[i]);
+    }
+    
+    cout<<"\nMinheap \n";
+    while(!pqmax3.empty()){
+        cout<<pqmax3.top()<<" ";
+        pqmax3.pop();
+    }
+  
+   
+return 0;	
+}
